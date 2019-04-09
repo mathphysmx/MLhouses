4449db8420f001bbd9ca8f9d617e391ee9828f30
Francisco Mendoza <mentofran@gmail.com>
2019-04-08 21:28:00 -0400

    project structure set

---
+++ src/Francisco_houses.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+train_data = pd.read_csv('data/raw/train.csv')
+test__data = pd.read_csv('data/raw/test.csv')
+
+
+from pyspark.sql import SparkSession
+from pyspark import SparkConf, SparkContext
+spark = SparkSession \
+.builder \
+.master("local")\
+.appName("FranciscoApp") \
+.getOrCreate()
+sc = SparkContext.getOrCreate(SparkConf().setAppName("my app").setMaster("local[*]"))
